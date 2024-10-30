58d8bb1d329c5172efbc54680f66bc042f5b3772
jacob <savefilestome@gmail.com>
2024-10-30 15:06:13 -0300

    finished all of the tables

--- SQL Assignment 3.sql
+++ SQL Assignment 3.sql
@@ -3,19 +3,19 @@
 
 CREATE TABLE Departments(
     departmentID INT,
-    departmentName VARCHAR(55) NOT NULL,
+    departmentName VARCHAR(64) NOT NULL,
     departmentLocation VARCHAR(150) NOT NULL,
-    PRIMARY KEY(departmentID)
+    PRIMARY KEY(departmentID),
 );
 
 CREATE TABLE Employees(
     employeeID INT,
-    employeeName VARCHAR(55) NOT NULL,
-    employeePosition VARCHAR(55) NOT NULL,
+    employeeName VARCHAR(64) NOT NULL,
+    employeePosition VARCHAR(64) NOT NULL,
     employeeSalary FLOAT NOT NULL,
     departmentID INT,
-    PRIMARY KEY(employeeID)
-    FOREIGN KEY(departmentID) REFERENCES Departments(departmentID)
+    PRIMARY KEY(employeeID),
+    FOREIGN KEY(departmentID) REFERENCES Departments(departmentID),
 );
 
 /* Example two */
@@ -23,7 +23,7 @@ CREATE TABLE Employees(
 
 CREATE TABLE Projects(
     projectID INT,
-    projectName VARCHAR(55) NOT NULL,
+    projectName VARCHAR(64) NOT NULL,
     projectStartDate DATE NOT NULL,
     taskID VARCHAR(150),
     PRIMARY KEY(projectID),
@@ -33,38 +33,122 @@ CREATE TABLE Tasks(
     taskID INT,
     taskDesc VARCHAR(150) NOT NULL,
     taskDueDate DATE,
-    projectID
-    PRIMARY KEY(taskID)
-    FOREIGN KEY projectID REFERENCES Projects(projectID)
+    projectID INT,
+    PRIMARY KEY(taskID),
+    FOREIGN KEY (projectID) REFERENCES Projects(projectID),
 );
 
 ALTER TABLE Projects
-FOREIGN KEY taskID REFERENCES Tasks(taskID);
+ADD FOREIGN KEY (taskID) REFERENCES Tasks(taskID);
 
 /* Example Three */
 
 CREATE TABLE Manufacturers(
     ManufacturerID INT,
-    ManufacturerName VARCHAR(55) NOT NULL,
-    ManufacturersCountry VARCHAR(55) NOT NULL,
+    ManufacturerName VARCHAR(64) NOT NULL,
+    ManufacturersCountry VARCHAR(64) NOT NULL,
     PRIMARY KEY(ManufacturerID)
 )
 
 CREATE TABLE Products(
     productID INT,
-    productName VARCHAR(55) NOT NULL,
+    productName VARCHAR(64) NOT NULL,
     productPrice FLOAT NOT NULL,
-    ManufacturerID
-    PRIMARY KEY(productID)
-    FOREIGN KEY ManufacturerID REFERENCES Manufacturers(ManufacturerID)
-)
+    ManufacturerID INT,
+    PRIMARY KEY(productID),
+    FOREIGN KEY (ManufacturerID) REFERENCES Manufacturers(ManufacturerID)
+);
 
 /* Example four */
 
 CREATE TABLE Authors(
-
-)
+    authorID INT,
+    authorName VARCHAR(64) NOT NULL,
+    authorEmail VARCHAR(64) NOT NULL UNIQUE,
+    PRIMARY KEY (authorID),
+);
 
 CREATE TABLE Posts(
-    
+    postID INT,
+    postTitle VARCHAR(55) NOT NULL,
+    postContent VARCHAR(250) NOT NULL,
+    authorID INT,
+    PRIMARY KEY (postID),
+    FOREIGN KEY (authorID) REFERENCES Authors(authorID),
+);
+
+/* example five */
+
+CREATE TABLE Memberships(
+    membershipID INT,
+    membershipType INT NOT NULL,
+    monthlyFee FLOAT NOT NULL,
+    PRIMARY KEY (membershipID)
+);
+
+CREATE TABLE Members(
+    memberID INT,
+    memberName VARCHAR(64) NOT NULL,
+    joinDate DATE NOT NULL,
+    membershipType INT NOT NULL,
+    PRIMARY KEY (memberID),
+    FOREIGN KEY (membershipType) REFERENCES Memberships(membershipType),
+);
+
+/* example six */
+
+CREATE TABLE Countries(
+    countryID INT,
+    countryName VARCHAR(64) NOT NULL,
+    coutryContinent VARCHAR(64) NOT NULL,
+    cityID INT NOT NULL,
+    PRIMARY KEY (countryID),
+);
+
+CREATE TABLE Cities(
+    cityID INT,
+    cityName VARCHAR(64) NOT NULL,
+    cityPopulation FLOAT NOT NULL,
+    countryID INT,
+    PRIMARY KEY (cityID),
+    FOREIGN KEY (countryID) REFERENCES Countries(countryID),
+);
+
+ALTER TABLE Countries
+ADD FOREIGN KEY (cityID) REFERENCES cities(cityID);
+
+/* example seven */
+
+CREATE TABLE Catagories(
+    catagoryID INT,
+    catagoryName VARCHAR(64) NOT NULL UNIQUE,
+    catagoryDesc TEXT NOT NULL,
+    PRIMARY KEY(catagoryID),
 )
+
+CREATE TABLE Products(
+    productID INT,
+    productName VARCHAR(64),
+    productPrice FLOAT NOT NULL,
+    catagoryID INT,
+    PRIMARY KEY(productID),
+    FOREIGN KEY (catagoryID) REFERENCES Catagories(catagoryID),
+)
+
+/* example eight */
+
+CREATE TABLE Departments(
+    departmentID INT,
+    departmentName VARCHAR(64) NOT NULL,
+    departmentBuilding VARCHAR(64) NOT NULL,
+    PRIMARY KEY(departmentID),
+);
+
+CREATE TABLE Professors(
+    professorID INT,
+    professorName VARCHAR(64) NOT NULL,
+    professorTitle VARCHAR(64) NOT NULL,
+    departmentID INT,
+    PRIMARY KEY (professorID),
+    FOREIGN KEY (departmentID) REFERENCES Departments(departmentID),
+);
